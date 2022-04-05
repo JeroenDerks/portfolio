42bf48f901c1d7e62c7e6abb6aa36599d7b78645
JeroenDerks <jeroen.derks@lau.do>
2022-04-05 20:18:31 +0100

    feat: merge with branch to install nextjs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0f9ae0d105bcd487"; }
+/******/ 		__webpack_require__.h = function() { return "f2e36ef35764fbac"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
